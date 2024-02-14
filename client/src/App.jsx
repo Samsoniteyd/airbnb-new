d9ba3fe04e3b6a2e63313593fcd10eedce6fd295
AMOO <amoosamson@gmail.com>
2024-02-14 06:28:07 +0100

    new upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
+import Register from './pages/Register';
 
 function App() {
   
@@ -10,8 +12,12 @@ function App() {
   return (
 
     <Routes>
+      <Route path='/' element={<Layout/>} >
+        
       <Route index element={< IndexPage />} />
       <Route path='/login' element={< LoginPage />} />
+      <Route path='/Register' element={< Register />} />
+        </Route>
     </Routes>
    
    
