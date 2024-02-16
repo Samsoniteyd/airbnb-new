1b36c0b438ae02cd4a8c73dccb58e8645b0b8cd0
AMOO <amoosamson@gmail.com>
2024-02-16 02:20:09 +0100

    new upload

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -4,14 +4,15 @@ import { Link } from 'react-router-dom'
 
 const Register = () => {
     const [name, setName] = useState('');
-    const [email, password] = useState('');
-    const 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
   return (
     <div className='mt-4 grow flex items-center justify-around'>
         <div className='mb-64'>
         <h1 className='text-4xl text-center mb-4'>Register</h1>
         <form action="" className='max-w-md mx-auto'>
-            <input type="text" placeholder='john wick' />
+            <input type="text" placeholder='john wick' value={name} onChange={ev => setName(ev.target.value)} />
             <input type="email" placeholder='your@email.com' />
             <input type="password"  placeholder='password' />
             <button className='primary'>login</button>
