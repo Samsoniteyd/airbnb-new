8ecb10e8f8c2e7c89fd4565fbab83748f83b0b82
AMOO <amoosamson@gmail.com>
2024-02-19 05:30:50 +0100

    new

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors( {
+    credentials: true,
+    origin: 'http://localhost:5173',
+
+} ));
 
 const port = process.env.PORT || 4000;
 
 app.get('/test', (req, res)=> {
     res.json('test ok');
 });
+
+app.post('/register')
 
 
 app.listen(port, ()=> {
